0c5a1b5f1dd08ae81fee5a1ad739cc53f5b1c9de
FlynntKnapp <bruce.stull@gmail.com>
2024-03-02 04:46:17 -0500

    In progress

--- sample_table_simple.py
+++ sample_table_simple.py
@@ -1,18 +1,9 @@
-import os
-
 from docx import Document
+
+from utils import delete_and_save_docx
 
 # Specify the file path for the .docx file
 file_path = "output/TableSimple.docx"
-
-# Delete the file if it exists
-# Check if the file exists to avoid FileNotFoundError
-if os.path.exists(file_path):
-    # Delete the file
-    os.remove(file_path)
-    print(f"File {file_path} deleted.")
-else:
-    print("The file does not exist.")
 
 # Create a new Document
 doc = Document()
@@ -22,17 +13,18 @@ doc.add_heading("The Official Table Example!", 0)
 
 # Create a list of records
 records = (
-    ("Skill 00", "Skill 01"),
-    ("Skill 10", "Skill 11"),
-    ("Skill 20", "Skill 21"),
+    ("Skill 00", "Skill 03", "Skill 06"),
+    ("Skill 01", "Skill 04", "Skill 07"),
+    ("Skill 02", "Skill 05", "Skill 08"),
 )
 
 # Add a table, which contains the records, to the document
-table = doc.add_table(rows=1, cols=2)
-for skill0, skill1 in records:
+table = doc.add_table(rows=1, cols=3)
+for col0, col1, col2 in records:
     row_cells = table.add_row().cells
-    row_cells[0].text = skill0
-    row_cells[1].text = skill1
+    row_cells[0].text = col0
+    row_cells[1].text = col1
+    row_cells[2].text = col2
 
 # Save the document to a .docx file
-doc.save(file_path)
+delete_and_save_docx(file_path, doc)
