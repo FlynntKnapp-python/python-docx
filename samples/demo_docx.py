584051204c6bf05b7eb4f4ab6b31b801f8d76613
FlynntKnapp <bruce.stull@gmail.com>
2024-04-06 13:24:14 -0400

    In progress

--- samples/demo_docx.py
+++ samples/demo_docx.py
@@ -1,9 +1,9 @@
 from docx import Document
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Inches
-from utils import create_docx_if_not_exists, delete_and_or_save_docx
+import utils
 
-# Specify the file path for the .docx file
+# Specify the file path for the .docx file:
 file_path = "samples/output/Demo.docx"
 
 document = Document()
@@ -58,4 +58,4 @@ third_page_lone_paragraph = document.add_paragraph(
 third_page_lone_paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
 
 # Save the document to a .docx file
-delete_and_or_save_docx(file_path, document)
+saved = utils.save_docx(file_path, document)
--- samples/heading_and_paragraph.py
+++ samples/heading_and_paragraph.py
@@ -1,21 +1,21 @@
 from docx import Document
-from utils import create_docx_if_not_exists, delete_and_or_save_docx
+import utils
 
-# Specify the file path for the .docx file
+# Specify the file path for the .docx file:
 file_path = "samples/output/HeadingAndParagraph.docx"
 
-# Create a new Document
+# Create a new Document:
 doc = Document()
 
-# Add a title to the document
+# Add a title to the document:
 h = doc.add_heading("Document Title", 0)
 print("Added h.text: \n", h.text)
 
-# Add a paragraph of text
+# Add a paragraph of text:
 p = doc.add_paragraph(
     "This is a simple paragraph that is being added to the document. "
 )
 print("Added p.text: \n", p.text)
 
-# Save the document to a .docx file
-delete_and_or_save_docx(file_path, doc)
+# Save the document to a .docx file:
+saved = utils.save_docx(file_path, doc)
