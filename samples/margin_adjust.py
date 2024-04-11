986e8ab8ecd3efbcd18529e99b45d792c6639256
FlynntKnapp <bruce.stull@gmail.com>
2024-04-11 15:26:45 -0400

    Update 'samples\margin_adjust.py'.

--- samples/margin_adjust.py
+++ samples/margin_adjust.py
@@ -28,5 +28,5 @@ print("Right Margin:", section.right_margin)
 
 
 # Save the document to a .docx file:
-saved = docx_builder.save_docx(file_path, doc)
+saved = docx_builder.manage_docx_file(file_path, doc, "save")
 print("Saved: ", saved)
