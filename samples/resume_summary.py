beea9523eabefc3e05a9f7ec49b100cdace4fc8f
FlynntKnapp <bruce.stull@gmail.com>
2024-04-11 15:42:56 -0400

    Removed code from 'samples\resume_summary.py'.

--- samples/resume_summary.py
+++ samples/resume_summary.py
@@ -4,19 +4,4 @@ from base import docx_builder
 from docx import Document
 from docx.text.paragraph import Paragraph
 
-
-def add(paragraph: Paragraph = None) -> Document:
-
-    if paragraph is None:
-        doc = Document()
-
-        # Add a horizontal line:
-        doc = docx_builder.insert_horizontal_line(doc)
-
-    paragraph = docx_builder.insert_horizontal_line_paragraph_top(paragraph)
-
-    return paragraph
-
-
-if __name__ == "__main__":
-    add()
+pass
