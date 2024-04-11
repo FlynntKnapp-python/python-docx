753f4354c0a50162c4fffd9b7527ffb36d5836d6
FlynntKnapp <bruce.stull@gmail.com>
2024-04-11 09:48:44 -0400

    Headings example in progress

---
+++ samples/c_headings.py
@@ -0,0 +1,13 @@
+# samples\c_headings.py
+
+from base.builder import DocxBuilder
+
+# Specify the file path for the .docx file:
+file_path = "samples/output/CEmpty.docx"
+
+# Create a new DocxBuilder:
+doc = DocxBuilder(file_path)
+
+# Save the document to a .docx file:
+saved = doc.save()
+print(f"Document saved: {saved}")
