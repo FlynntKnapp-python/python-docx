d7f36f84fbafc347f4740791b0771c25a9edd764
FlynntKnapp <bruce.stull@gmail.com>
2024-04-11 15:23:28 -0400

    Update 'samples\horizontal_line_example.py'.

--- samples/horizontal_line_example.py
+++ samples/horizontal_line_example.py
@@ -13,18 +13,18 @@ doc = Document()
 paragraph = doc.add_paragraph()
 paragraph.add_run("This is the text above the first line.")
 
-# Add a horizontal line:
+# Add a horizontal line to the bottom of the paragraph:
 paragraph = docx_builder.insert_horizontal_line_paragraph_bottom(paragraph)
 
 # Add another paragraph after the line:
 paragraph_after = doc.add_paragraph("This is the text below the first line.")
 
-# Add another horizontal line:
+# Add a stand-alone horizontal line:
 doc = docx_builder.insert_horizontal_line(doc)
 
 # Add another paragraph after the line:
 paragraph_after = doc.add_paragraph("This is the text below the second line.")
 
 # Save the document:
-saved = docx_builder.save_docx(file_path, doc)
+saved = docx_builder.manage_docx_file(file_path, doc, "save")
 print("Saved: ", saved)
