a13ae7d437e00883f082a0fdbdd47a27945a3ee6
FlynntKnapp <bruce.stull@gmail.com>
2024-04-11 15:19:23 -0400

    Update 'samples\horizontal_line.py'.

--- samples/horizontal_line.py
+++ samples/horizontal_line.py
@@ -4,23 +4,20 @@ from base import docx_builder
 from docx import Document
 from docx.text.paragraph import Paragraph
 
+file_path = "samples/output/HorizontalLine.docx"
 
-def add(
-    file_path="samples/output/HorizontalLine.docx", paragraph: Paragraph = None
-) -> Document:
+# Create a new Document:
+doc = Document()
 
-    if paragraph is None:
-        doc = Document()
+# Add a paragraph to the document:
+p1 = doc.add_paragraph("This is a paragraph.")
 
-    # Add a horizontal line:
-    doc = docx_builder.insert_horizontal_line(doc)
+# Add a horizontal line:
+doc = docx_builder.insert_horizontal_line(doc)
 
-    # Save the document:
-    saved = docx_builder.save_docx(file_path, doc)
-    print("Saved: ", saved)
+# Add another paragraph to the document:
+p2 = doc.add_paragraph("This is another paragraph.")
 
-    return doc
-
-
-if __name__ == "__main__":
-    add()
+# Save the document:
+saved = docx_builder.save_docx(file_path, doc)
+print("Saved: ", saved)
